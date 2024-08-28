5f7f7914dfcd0d5ae75f6cc5ed5697ee5b3a9ec2
Noah <noahncassutti@gmail.com>
2024-08-28 00:09:28 -0300

    cambios

---
+++ Autores.py
@@ -0,0 +1,6 @@
+# AUTORES TP PROGRAMACION
+# -Avalos Victoria Inés 44519367
+# -Peralta Cassutti Noah Nicanor 42854566
+# -Gabriela María Sanchez 39860262
+# -Estrella Cardozo dni 39513526
+# -Bertorello Santiago Nicolas 38110423
